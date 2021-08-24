d5b7bff6955143c3246e3cd19aaeb69affac4c1b
NATFINEST <ese.nat4@gmail.com>
2021-08-24 13:01:16 +0100

    Create BookList component and fetch data from json-server(stub server)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Typography } from '@material-ui/core';
 import BookList from './BookList/BookList';
 
 const App = () => {
-  const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get('http://localhost:8000/books');
+      setBooks(res.data);
+    };
+
+    fetchBooks();
+  }, []);
+
   return (
     <div>
       <Typography variant="h2" component="h2" data-test="heading">
